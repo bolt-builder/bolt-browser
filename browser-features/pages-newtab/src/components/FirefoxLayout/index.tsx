f9547b434b637751b1b711fc45770469f2668abd
DevFlex-AI <mahirusus@gmail.com>
2026-08-09 19:30:50 +0000

    rebrand: Bolt logo and wordmark on start page instead of runtime Floorp branding

--- browser-features/pages-newtab/src/components/FirefoxLayout/index.tsx
+++ browser-features/pages-newtab/src/components/FirefoxLayout/index.tsx
@@ -1,16 +1,16 @@
+import boltIcon from "@/assets/bolt/Bolt_Icon.png";
+
 export function FirefoxLayout() {
   return (
     <div className="flex justify-center items-center mb-8">
       <img
-        src="chrome://branding/content/about-logo.png"
+        src={boltIcon}
         alt="Logo"
         className="w-16 h-16 mr-4"
       />
-      <img
-        src="chrome://branding/content/firefox-wordmark.svg"
-        alt="Wordmark"
-        className="h-12"
-      />
+      <span className="text-5xl font-semibold text-white drop-shadow-lg">
+        Bolt
+      </span>
     </div>
   );
 }
